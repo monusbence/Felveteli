71424f678fd555b2e052b819180823d473fd9459
monusbence <72020098+monusbence@users.noreply.github.com>
2024-02-07 13:01:12 +0100

    Add files via upload

--- web/script.js
+++ web/script.js
@@ -238,3 +238,10 @@ function displayTable(filteredData) {
 
 
 displayTable(adat);
+
+window.addEventListener('load', function () {
+    // Hide the loading overlay after 5 seconds
+    setTimeout(function () {
+        document.getElementById('loading-overlay').style.display = 'none';
+    }, 3000);
+});
--- web/script2.js
+++ web/script2.js
@@ -267,3 +267,10 @@ function updateSearchResults(filteredData) {
 }
 
 displayCards(adat);
+
+window.addEventListener('load', function () {
+    // Hide the loading overlay after 5 seconds
+    setTimeout(function () {
+        document.getElementById('loading-overlay').style.display = 'none';
+    }, 3000);
+});
