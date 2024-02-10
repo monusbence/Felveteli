98b5fe3f3ca6d172c37978dbaa667d20f8474940
monusbence <72020098+monusbence@users.noreply.github.com>
2024-02-10 22:28:19 +0100

    Add files via upload

---
+++ web/foOldal.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', function () {
+    // Hide the loading overlay after 5 seconds
+    setTimeout(function () {
+        document.getElementById('loading-overlay').style.display = 'none';
+    }, 2000);
+});
